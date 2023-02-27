72e88b2d66131dc65f8ecee88c14880228baa91e
heyimcsy <cinnamon1995@naver.com>
2023-02-27 11:31:39 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ const App = () => {
     { id: 3, age:21, name: "김유정"},
     { id: 4, age:29, name: "구교환"},
   ];
-  return <div className="style">
-    <div className='box-style'>
-      {users[0].age}-{users[0].name}</div>
-    <div className='box-style'>
-      {users[1].age}-{users[1].name}</div>
-    <div className='box-style'>
-      {users[2].age}-{users[2].name}</div>
-    <div className='box-style'>
-      {users[3].age}-{users[3].name}</div>
-  </div>;
-}
+  return (
+    <div className="app-style">
+      {users.map((item) => {
+        return (
+          <div key={item.id} className='box-style'>
+            {item.age} - {item.name}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default App;
