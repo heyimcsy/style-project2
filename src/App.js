9e42b4030d203417a6f469a28cf85a7259df6f62
heyimcsy <cinnamon1995@naver.com>
2023-02-27 11:55:38 +0900

    input 값

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  const users = [
-    { id: 1, age:30, name: "송중기"},
-    { id: 2, age:24, name: "송강"},
-    { id: 3, age:21, name: "김유정"},
-    { id: 4, age:29, name: "구교환"},
-  ];
+  const [users, setUsers] = useState([
+    { id: 1, age: 30, name: "송중기" },
+    { id: 2, age: 24, name: "송강" },
+    { id: 3, age: 21, name: "김유정" },
+    { id: 4, age: 29, name: "구교환" },
+  ]);
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+
   return (
-    <div className="app-style">
-      {users.map((item) => {
-        return (
-          <div key={item.id} className='box-style'>
-            {item.age} - {item.name}
-          </div>
-        );
-      })}
+    <div>
+      <div>
+      이름 : &nbsp;
+      <input 
+        value = {name}
+        onChange = {(event)=>{
+          setName(event.target.value);
+        }}
+        /> <br />
+      나이 : &nbsp; 
+      <input 
+        value = {age}
+        onChange = {(event)=>{
+          setAge(event.target.value);
+        }}/>
+      </div>
+      <div className="app-style">
+        {users.map((item) => {
+          return (
+            <div key={item.id} className="box-style">
+              {item.age} - {item.name}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
